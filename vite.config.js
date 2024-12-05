2a2ff57730e0fe1a3390c38efb60e5f7a4318bf4
dodonot3 <dodonot3.0@gmail.com>
2024-12-05 17:30:01 +0900

    배포수정

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/todo/', // 배포할 GitHub Pages 리포지토리 이름으로 변경, base 값은 항상 슬래시(/)로 시작하고 종료해야 함
+  // base: '/goorm-todo/', // 배포할 GitHub Pages 리포지토리 이름으로 변경, base 값은 항상 슬래시(/)로 시작하고 종료해야 함
 });
